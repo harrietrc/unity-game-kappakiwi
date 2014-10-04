322d41f5dff2a7b25dfe7f1cac10b0c0a06ca9b9
clee842 <clee842@aucklanduni.ac.nz>
2014-10-04 23:57:41 +1300

    start PlatformFactory

---
+++ Assets/Scripts/Common/PlatformFactory.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/** Create different types of platforms
+ * Normal bouncy platform, platform that crumbles
+ **/
+public class PlatformFactory : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
