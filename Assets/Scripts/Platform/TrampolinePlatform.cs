f81f01b023357f158d933da44d404e918911a02f
matthew W <bwan210@aucklanduni.ac.nz>
2014-10-05 18:30:15 +1300

    changed platform prefab scripts to correct ones

--- Assets/Scripts/Platform/TrampolinePlatform.cs
+++ Assets/Scripts/Platform/TrampolinePlatform.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class NewBehaviourScript : Platform {
+public class TrampolinePlatform : Platform {
 
 	// Use this for initialization
 	void Start () {
