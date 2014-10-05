2f3e9ff9e61b25bdb01bf1331e2053830cc17f70
gazza6 <lshgary@hotmail.com>
2014-10-06 11:29:44 +1300

    added a welcome screen

--- Assets/Scripts/Screens/welcome.cs
+++ Assets/Scripts/Screens/welcome.cs
@@ -12,4 +12,20 @@ public class welcome : MonoBehaviour {
 	void Update () {
 	
 	}
+
+	void OnGUI(){
+		GUI.skin.label.fontSize = 30;
+		
+		GUI.skin.label.alignment = TextAnchor.LowerCenter;
+		
+		GUI.Label (new Rect(Screen.width * 0.1f, Screen.height * 0.05f, Screen.width * 0.8f, Screen.height * 0.4f), "Kappa Kiwi");
+		
+		if (GUI.Button (new Rect (Screen.width * 0.4f, Screen.height * 0.5f, Screen.width * 0.2f, Screen.height * 0.1f), "Play")) {
+			Application.LoadLevel("LevelSelection");		
+		}
+		
+		GUI.Button (new Rect(Screen.width * 0.4f, Screen.height * 0.65f, Screen.width * 0.2f, Screen.height * 0.1f), "Achievement");
+		
+		GUI.Button (new Rect(Screen.width * 0.4f, Screen.height * 0.8f, Screen.width * 0.2f, Screen.height * 0.1f), "Setting");
+	}
 }
