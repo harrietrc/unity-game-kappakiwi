40660a576757aa928b909e71a8213938fbe49aad
matthew W <bwan210@aucklanduni.ac.nz>
2014-10-06 16:52:56 +1300

    factory can now be called to generate

--- Assets/Scripts/Common/GameObjectFactory.cs
+++ Assets/Scripts/Common/GameObjectFactory.cs
@@ -3,6 +3,12 @@ using System.Collections;
 
 public class GameObjectFactory : MonoBehaviour {
 
+	private GameObject newPlatform;
+	private int ypos = -2;
+	private int xpos = 2;
+
+	private RNGStateGenerator;
+
 	// Use this for initialization
 	void Start () {
 	
@@ -10,6 +16,23 @@ public class GameObjectFactory : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+
 	}
+
+	public void setRNGDependency(RNGStateGenerator dependency){
+		this.setRNGDependency = dependency;
+		}
+
+	public void generateTick(){
+		if (1 == 1) {
+			this.newPlatform = (GameObject) Instantiate(Resources.Load ("Prefabs/Platforms/" + "pref_standard_platform"));
+			this.newPlatform.transform.position = new Vector3(xpos,ypos,0);
+			ypos = ypos + 1;
+			xpos = xpos + 1;
+				}
+
+		}
+
+	public GameObjectFactory(){
+		}
 }
--- Assets/Scripts/Common/PlayerController.cs
+++ Assets/Scripts/Common/PlayerController.cs
@@ -7,6 +7,12 @@ public class PlayerController : MonoBehaviour {
 	private int selectedId;
 	private int speed = 5;
 
+	private GameObject currentPlatform;
+
+	private ArrayList visitedPlatforms = new ArrayList();
+	private GameObjectFactory factory = new GameObjectFactory();
+
+
 	// Use this for initialization
 	void Start () {
 	
@@ -26,7 +32,15 @@ public class PlayerController : MonoBehaviour {
 
 	}
 
+	public void setFactoryDependency(GameObjectFactory dependency){
+		this.factory = dependency;
+		}
+
 	void OnCollisionEnter2D(Collision2D coll) {
+				if (!visitedPlatforms.Contains(coll.gameObject)) {
+					factory.generateTick();
+					visitedPlatforms.Add(coll.gameObject);
+				}
 				if (coll.gameObject.tag == "platform" && this.transform.position.y > coll.gameObject.transform.position.y) {
 						rigidbody2D.velocity = Vector2.zero;
 						rigidbody2D.AddForce (jumpForce);
