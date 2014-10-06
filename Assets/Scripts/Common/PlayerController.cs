c2db6b0c54e4bd6de0796b4a041ccefe5c534cc9
matthew W <bwan210@aucklanduni.ac.nz>
2014-10-06 15:51:57 +1300

    added movement and bouncing

--- Assets/Scripts/Common/PlayerController.cs
+++ Assets/Scripts/Common/PlayerController.cs
@@ -3,13 +3,35 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
+	private Vector2 jumpForce = new Vector2(0, 800);
+	private int selectedId;
+	private int speed = 5;
+
 	// Use this for initialization
 	void Start () {
 	
 	}
+
 	
-	// Update is called once per frame
-	void Update () {
-	
+	void Update ()
+	{
+		if (Input.GetKey(KeyCode.LeftArrow))
+		{
+			transform.position += Vector3.left * speed * Time.deltaTime;
+		}
+		if (Input.GetKey(KeyCode.RightArrow))
+		{
+			transform.position += Vector3.right * speed * Time.deltaTime;
+		}
+
+	}
+
+	void OnCollisionEnter2D(Collision2D coll) {
+		if (coll.gameObject.name == "pref_standard_platform" && this.transform.position.y > coll.gameObject.transform.position.y) {
+			rigidbody2D.velocity = Vector2.zero;
+			rigidbody2D.AddForce(jumpForce);
+		}
+
+		
 	}
 }
