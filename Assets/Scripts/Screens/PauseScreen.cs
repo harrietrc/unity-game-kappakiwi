9d0710b90ed34f597cf319e787818d50222125fe
gazza6 <lshgary@hotmail.com>
2014-10-07 10:17:17 +1300

    pause works on computer

---
+++ Assets/Scripts/Screens/PauseScreen.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PauseScreen : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnGUI(){
+
+		if (GUI.Button (new Rect (Screen.width * 0.4f, Screen.height * 0.3f, Screen.width * 0.2f, Screen.height * 0.1f), "Resume")) {
+			Application.LoadLevel("scn_mock");
+		}
+		
+		if (GUI.Button (new Rect (Screen.width * 0.4f, Screen.height * 0.7f, Screen.width * 0.2f, Screen.height * 0.1f), "Back to menu")) {
+			Application.LoadLevel("WelcomeScreen");		
+		}
+	}
+}
---
+++ Assets/Scripts/Screens/pauseControl.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class pauseControl : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		DesktopInput ();
+	}
+
+	void DesktopInput(){
+		if (Input.GetMouseButton (0)) {
+			Application.LoadLevel("PauseScreen");		
+		}
+	}
+	
+}
