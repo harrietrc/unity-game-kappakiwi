a97bd31a0629bfa1675d52955e40f1e53bbd12a0
crazyRam <niyazi_oz@hotmail.com>
2014-10-08 10:31:12 +1300

    RNG modified, methods added
    Random vector creation ..

--- Assets/Scripts/RNG/RNGStateGenerator.cs
+++ Assets/Scripts/RNG/RNGStateGenerator.cs
@@ -1,15 +1,54 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class RNGStateGenerator : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
+public class RNGStateGenerator {
 	
+	// Note: Random.Range(min, max) 
+	// has overloaded methods, that returns the value in the both float and int
+	
+	public int rangeInt(int min, int max){
+		// returns a random value of integer within given the range
+		
+		// formula of creating number in a range is 
+		// (int)(random() * (max - min) + min)
+		
+		int z = (int) ( Random.value * (max - min) + min);
+		return z;
+		// ex: range is 5 and 9, value returns always between [0.0, 1.0)
+		// say that upper limit very close to 1, for ex. 0.9999999, take it as 1
+		// z = ( 1 * (9-5) + 5) --> yields to = 9 
+		// if random value is 0, then low value is 5
 	}
 	
-	// Update is called once per frame
-	void Update () {
-	
+	public Vector2 getRandomV2(){
+		// returns a Vector2 that x and y in range of -10 , +10
+		Vector2  randomV2 = new Vector2(Random.Range(-10.0, 10.0), 0, Random.Range(-10.0, 10.0));
+		return randomV2;
 	}
+	
+	public Vector2 getRandomV2(float x, float y){
+		//returns a Vector2 instance given range of x, y float values
+		Vector2 randomV2 = new Vector2(Random.Range(x, y), 0, Random.Range(x, y));
+		return randomV2;
+	}
+	
+	public Vector3 getRandomV3(float x, float y){
+		// returns aVector3 instance of x, y and z=0 values
+		Vector2 randomV3 = new Vector3(Random.Range(x, y), 0, Random.Range(x, y), 0);
+		return randomV3;
+	}
+	
+	public Vector3 getRandomV3(float x, float y, float z){
+		// returns aVector3 instance of x, y and z values
+		Vector2 randomV3 = new Vector3(Random.Range(x, y), 0, Random.Range(x, y), Random.Range(x, y));
+		return randomV3;
+	}
+	
+	
+	
+	public int getRandomInRange(int min, int max){
+		
+		
+	}
+	
 }
