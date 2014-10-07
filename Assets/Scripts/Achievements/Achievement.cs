9621cbef53728ad10792e3793313e7ea1ededaea
Earl Balmes <ebal155@aucklanduni.ac.nz>
2014-10-07 15:45:03 +1300

    Added persistence and inheritance to achievements

---
+++ Assets/Scripts/Achievements/Achievement.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class Achievements {
+
+	protected bool isUnlocked;
+	protected string key;
+
+	public string getKey() {
+		return this.key;
+	}
+
+	abstract public bool isAchievementUnlocked ();
+}
--- Assets/Scripts/Achievements/PlatformAchievement.cs
+++ Assets/Scripts/Achievements/PlatformAchievement.cs
@@ -1,34 +1,31 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlatformAchievement : MonoBehaviour {
-
-	private bool isUnlocked;
+public class PlatformAchievement : Achievements {
+	
 	private int countToUnlock;
-	private string message;
 	private static int count;
 
-	public	PlatformAchievement(string message, int countToUnlock) {
+	public	PlatformAchievement(string key, int countToUnlock) {
 		this.isUnlocked = false;
 		count = 0;
 		this.countToUnlock = countToUnlock;
-		this.message = message;
+		this.key = key;
 	}
 
-	public bool IsAchievementUnlocked() {
+	public override bool isAchievementUnlocked() {
 		if (count >= this.countToUnlock && !this.isUnlocked) {
 			this.isUnlocked = true;
-			displayMessage();
 		}
-
 		return this.isUnlocked;
 	}
 
 	public void displayMessage() {
-		Debug.Log ("Achievement Unlocked: " + this.message);
+		Debug.Log ("Achievement Unlocked: " + this.key);
 	}
 
 	public static void incrementCount() {
 		count++;;
 	}
+
 }
