c59ff8785971e577c1bc8344cfd37364274bf8fd
Matthew W <bwan210@aucklanduni.ac.nz>
2014-10-08 17:04:51 +1300

    level selection work

--- Assets/Scripts/Screens/levelSelection.cs
+++ Assets/Scripts/Screens/levelSelection.cs
@@ -20,24 +20,53 @@ public class levelSelection : MonoBehaviour {
 		GUI.skin.label.alignment = TextAnchor.LowerCenter;
 
 		if (GUI.Button (new Rect (Screen.width * 0.1f, Screen.height * 0.05f, Screen.width * 0.8f, Screen.height * 0.2f), "Endless")) {
+			LevelSelection.CURRENT_THEME = Theme.endless;
+			LevelSelection.LEVEL = 1;
 			Application.LoadLevel("scn_mock");	
 		}
 
 		GUI.Label (new Rect(Screen.width * 0.1f, Screen.height * 0.35f, Screen.width * 0.25f, Screen.height * 0.2f), "Story");
 
+
+
+		if (GUI.Button (new Rect(Screen.width * 0.35f, Screen.height * 0.35f, Screen.width * 0.1f, Screen.height * 0.2f), "1")) {
+			LevelSelection.CURRENT_THEME = Theme.story;
+			LevelSelection.LEVEL = 1;
+			Application.LoadLevel("scn_mock");	
+		}
+
+		if (GUI.Button (new Rect(Screen.width * 0.55f, Screen.height * 0.35f, Screen.width * 0.1f, Screen.height * 0.2f), "2")) {
+			LevelSelection.CURRENT_THEME = Theme.story;
+			LevelSelection.LEVEL = 2;
+			Application.LoadLevel("scn_mock");	
+		}
+
+		if (GUI.Button (new Rect(Screen.width * 0.75f, Screen.height * 0.35f, Screen.width * 0.1f, Screen.height * 0.2f), "3")) {
+			LevelSelection.CURRENT_THEME = Theme.story;
+			LevelSelection.LEVEL = 3;
+			Application.LoadLevel("scn_mock");	
+		}
+
+
 		GUI.Label (new Rect(Screen.width * 0.1f, Screen.height * 0.65f, Screen.width * 0.25f, Screen.height * 0.2f), "Xmas");
 
-		GUI.Button (new Rect(Screen.width * 0.35f, Screen.height * 0.35f, Screen.width * 0.1f, Screen.height * 0.2f), "1");
+		if (GUI.Button (new Rect (Screen.width * 0.35f, Screen.height * 0.75f, Screen.width * 0.1f, Screen.height * 0.2f), "1")) {
+			LevelSelection.CURRENT_THEME = Theme.xmas;
+			LevelSelection.LEVEL = 1;
+			Application.LoadLevel("scn_mock");	
+				}
 
-		GUI.Button (new Rect(Screen.width * 0.55f, Screen.height * 0.35f, Screen.width * 0.1f, Screen.height * 0.2f), "2");
+		if (GUI.Button (new Rect (Screen.width * 0.55f, Screen.height * 0.75f, Screen.width * 0.1f, Screen.height * 0.2f), "2")) {
+			LevelSelection.CURRENT_THEME = Theme.xmas;
+			LevelSelection.LEVEL = 2;
+			Application.LoadLevel("scn_mock");	
+				};
 
-		GUI.Button (new Rect(Screen.width * 0.75f, Screen.height * 0.35f, Screen.width * 0.1f, Screen.height * 0.2f), "3");
-
-		GUI.Button (new Rect(Screen.width * 0.35f, Screen.height * 0.75f, Screen.width * 0.1f, Screen.height * 0.2f), "1");
-
-		GUI.Button (new Rect(Screen.width * 0.55f, Screen.height * 0.75f, Screen.width * 0.1f, Screen.height * 0.2f), "2");
-
-		GUI.Button (new Rect(Screen.width * 0.75f, Screen.height * 0.75f, Screen.width * 0.1f, Screen.height * 0.2f), "3");
+		if (GUI.Button (new Rect (Screen.width * 0.75f, Screen.height * 0.75f, Screen.width * 0.1f, Screen.height * 0.2f), "3")) {
+			LevelSelection.CURRENT_THEME = Theme.xmas;
+			LevelSelection.LEVEL = 3;
+			Application.LoadLevel("scn_mock");	
+				};
 
 	}
 }
