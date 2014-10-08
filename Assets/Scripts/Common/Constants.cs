bba28993c57a9963ffbe4c8ffdc1e42d90f2e2eb
Sean <toyman93@gmail.com>
2014-10-08 16:55:49 +1300

    Resolving merge conflicts

--- Assets/Scripts/Common/Constants.cs
+++ Assets/Scripts/Common/Constants.cs
@@ -3,6 +3,10 @@ using System.Collections;
 
 public class Constants {
 
+	public static float SMALL_SHIFT = -.4f;
+	public static float MEDIUM_SHIFT = -.6f;
+	public static float LARGE_SHIFT = -.8f;
+
 	private Constants(){
 		}
 
--- Assets/Scripts/Common/GameObjectFactory.cs
+++ Assets/Scripts/Common/GameObjectFactory.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+
 public class GameObjectFactory : MonoBehaviour {
 
 	private GameObject newPlatform;
@@ -57,13 +58,13 @@ public class GameObjectFactory : MonoBehaviour {
 	private bool temp = true;
 	private void hardCodedGenerateTickHook(){	
 		if (temp) {
-
+			if (Application.loadedLevelName != "level_one") {
 						this.newPlatform = (GameObject)Instantiate (Resources.Load ("Prefabs/Platforms/" + "pref_standard_platform"));
 						this.newPlatform.transform.position = new Vector3 (2, 9, 0);
 
 						this.newPlatform = (GameObject)Instantiate (Resources.Load ("Prefabs/Platforms/" + "pref_standard_platform"));
 						this.newPlatform.transform.position = new Vector3 (-2, 12, 0);
-
+			}
 				} else {
 					
 				}
