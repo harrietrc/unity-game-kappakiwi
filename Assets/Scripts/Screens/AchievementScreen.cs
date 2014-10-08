3824efd7980763dbe9439bdbc4b15adf15c8ea87
Earl Balmes <ebal155@aucklanduni.ac.nz>
2014-10-08 13:03:39 +1300

    Finished achievement screen to show locked and unlocked achievements
    Currently mostly hardcoded - will fix later after modifying
    AchievementManager

--- Assets/Scripts/Screens/AchievementScreen.cs
+++ Assets/Scripts/Screens/AchievementScreen.cs
@@ -14,18 +14,56 @@ public class AchievementScreen : MonoBehaviour {
 	}
 	
 	void OnGUI(){
+
+		GUIStyle lockedStyle = new GUIStyle (GUI.skin.label);
+		lockedStyle.normal.textColor = Color.black;
 		
-		GUI.contentColor = Color.grey;
-		
-		GUI.Label (new Rect(Screen.width * 0.1f, Screen.height * 0.2f, Screen.width * 0.8f, Screen.height * 0.1f), "Get 20 points in a single game");
-		
-		GUI.Label (new Rect(Screen.width * 0.1f, Screen.height * 0.3f, Screen.width * 0.8f, Screen.height * 0.1f), "Get 40 points in a single game");
-		
-		GUI.Label (new Rect(Screen.width * 0.1f, Screen.height * 0.4f, Screen.width * 0.8f, Screen.height * 0.1f), "Get 60 points in a single game");
-		
-		GUI.Label (new Rect(Screen.width * 0.1f, Screen.height * 0.5f, Screen.width * 0.8f, Screen.height * 0.1f), "Get 80 points in a single game");
-		
-		GUI.Label (new Rect(Screen.width * 0.1f, Screen.height * 0.6f, Screen.width * 0.8f, Screen.height * 0.1f), "Get 100 points in a single game");
+		GUIStyle unlockedStyle = new GUIStyle (GUI.skin.label);
+		unlockedStyle.normal.textColor = Color.white;
+
+		float highscore = PlayerPrefs.GetFloat ("HighScore");
+
+		GUI.Label (new Rect (Screen.width * 0.1f, Screen.height * 0.05f, Screen.width * 0.8f, Screen.height * 0.1f), "High score: " + highscore, unlockedStyle);
+
+		if (PlayerPrefs.GetInt ("Bounced on 20 platforms!") == 1) {
+			GUI.Label (new Rect (Screen.width * 0.1f, Screen.height * 0.2f, Screen.width * 0.8f, Screen.height * 0.1f), "Get 20 points in a single game", unlockedStyle);
+			Debug.Log ("Here at white");
+		} else {
+			GUI.Label (new Rect (Screen.width * 0.1f, Screen.height * 0.2f, Screen.width * 0.8f, Screen.height * 0.1f), "Get 20 points in a single game", lockedStyle);
+			Debug.Log ("Here at grey");
+		}
+
+		if (PlayerPrefs.GetInt ("Bounced on 40 platforms!") == 1) {
+			GUI.Label (new Rect (Screen.width * 0.1f, Screen.height * 0.3f, Screen.width * 0.8f, Screen.height * 0.1f), "Get 40 points in a single game", unlockedStyle);
+			Debug.Log ("Here at white");
+		} else {
+			GUI.Label (new Rect (Screen.width * 0.1f, Screen.height * 0.3f, Screen.width * 0.8f, Screen.height * 0.1f), "Get 40 points in a single game", lockedStyle);
+			Debug.Log ("Here at grey");
+		}	
+
+		if (PlayerPrefs.GetInt ("Bounced on 60 platforms!") == 1) {
+			GUI.Label (new Rect (Screen.width * 0.1f, Screen.height * 0.4f, Screen.width * 0.8f, Screen.height * 0.1f), "Get 60 points in a single game", unlockedStyle);
+			Debug.Log ("Here at white");
+		} else {
+			GUI.Label (new Rect (Screen.width * 0.1f, Screen.height * 0.4f, Screen.width * 0.8f, Screen.height * 0.1f), "Get 60 points in a single game", lockedStyle);
+			Debug.Log ("Here at grey");
+		}	
+
+		if (PlayerPrefs.GetInt ("Bounced on 80 platforms!") == 1) {
+			GUI.Label (new Rect (Screen.width * 0.1f, Screen.height * 0.5f, Screen.width * 0.8f, Screen.height * 0.1f), "Get 80 points in a single game", unlockedStyle);
+			Debug.Log ("Here at white");
+		} else {
+			GUI.Label (new Rect (Screen.width * 0.1f, Screen.height * 0.5f, Screen.width * 0.8f, Screen.height * 0.1f), "Get 80 points in a single game", lockedStyle);
+			Debug.Log ("Here at grey");
+		}	
+
+		if (PlayerPrefs.GetInt ("Bounced on 100 platforms!") == 1) {
+			GUI.Label (new Rect (Screen.width * 0.1f, Screen.height * 0.6f, Screen.width * 0.8f, Screen.height * 0.1f), "Get 100 points in a single game", unlockedStyle);
+			Debug.Log ("Here at white");
+		} else {
+			GUI.Label (new Rect (Screen.width * 0.1f, Screen.height * 0.6f, Screen.width * 0.8f, Screen.height * 0.1f), "Get 100 points in a single game", lockedStyle);
+			Debug.Log ("Here at grey");
+		}	
 		
 		if (GUI.Button (new Rect (Screen.width * 0.4f, Screen.height * 0.8f, Screen.width * 0.2f, Screen.height * 0.1f), "Back")) {
 			Application.LoadLevel("WelcomeScreen");	
