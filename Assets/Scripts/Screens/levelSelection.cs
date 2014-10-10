ef4f7081b0791df9ba726e6dbabd23552184d50a
matthew W <bwan210@aucklanduni.ac.nz>
2014-10-10 16:17:28 +1300

    level selection now persists to LeveSelection.cs

--- Assets/Scripts/Screens/levelSelection.cs
+++ Assets/Scripts/Screens/levelSelection.cs
@@ -11,11 +11,13 @@ public class levelSelection : MonoBehaviour {
 	
 	void OnGUI(){
 		// Create an invisible button and handle activity
-		if (GUI.Button (new Rect (548, 140, 50, 50), "", invisible)) {
+		if (GUI.Button (new Rect (548, 140, 50, 50), "test", invisible)) {
+			LevelSelection.LEVEL = 1;
 			Application.LoadLevel("level_one");
 		}
 		// Create an invisible button and handle activity
-		if (GUI.Button (new Rect (655, 140, 50, 50), "", invisible)) {
+		if (GUI.Button (new Rect (655, 140, 50, 50), "test2", invisible)) {
+			LevelSelection.LEVEL = 2;
 			Application.LoadLevel("level_two");
 		}
 		if (GUI.Button (new Rect (380, 600, 100, 50), "Back")) {
