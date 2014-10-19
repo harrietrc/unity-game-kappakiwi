137e8d302b44904dfedf225dba250921cfd795c6
MShehadeh <mshe246@gmail.com>
2014-10-20 11:21:49 +1300

    Set gravity scale for falling enemy from script.
    This is to ensure it sticks.

--- Assets/Scripts/Enemy/FallingEnemy.cs
+++ Assets/Scripts/Enemy/FallingEnemy.cs
@@ -7,6 +7,7 @@ public class FallingEnemy : Enemy {
 	// Use this for initialization
 	void Start () {
 		gameObject.rigidbody2D.velocity	= new Vector2(0,-7);
+		gameObject.rigidbody2D.gravityScale = 0;
 	}
 	
 	// Update is called once per frame
