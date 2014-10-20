36792f622e7147bb61bcf6f7c03dd5b98c8b25fd
Harriet Robinson-Chen <harrietrc@gmail.com>
2014-10-21 07:58:54 +1300

    Added the settings screen to the build settings
    Still working on settings screen graphical stuff, but the button on the
    welcome screen works now.

--- Assets/Scripts/Screens/settings.cs
+++ Assets/Scripts/Screens/settings.cs
@@ -5,11 +5,13 @@ public class settings : MonoBehaviour {
 
 	private Vector2 scrollViewVector = Vector2.zero;
 	public static string[] list = {"Standard", "Christmas"};
-	
+	public Texture2D backToMenu;
+
 	int indexNumber;
 	bool show = false;
 	
 	void OnGUI(){
+				GUI.backgroundColor = new Color(0, 0, 0, 0); // Hide the rectangles for buttons
 				GUIStyle style = new GUIStyle (GUI.skin.label);
 		
 				style.font = (Font)Resources.Load ("font/Animated");
@@ -20,7 +22,7 @@ public class settings : MonoBehaviour {
 		
 				GUI.Label (new Rect (Screen.width * 0.1f, Screen.height * 0.35f, Screen.width * 0.25f, Screen.height * 0.2f), "Current theme:", style);
 
-				if (GUI.Button (new Rect (Screen.width * 0.4f, Screen.height * 0.85f, Screen.width * 0.2f, Screen.height * 0.1f), "Back to menu", style)) {
+				if (GUI.Button (new Rect (Screen.width * 0.3f, Screen.height * 0.85f, Screen.width * 0.4f, Screen.height * 0.2f), backToMenu)) {
 						Application.LoadLevel ("WelcomeScreen");		
 				}
 
