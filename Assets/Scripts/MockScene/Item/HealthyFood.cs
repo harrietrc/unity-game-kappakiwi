d7c1ab7b0dd29d048a2c9e878548b15da694a784
Harriet Robinson-Chen <harrietrc@gmail.com>
2014-10-20 13:08:04 +1300

    New Xmas sprites integrated into game
    Switched by setting enum field. I think the spritesheet slicing may be
    stored in the .meta files (or at least that's where it will be if you
    pull this and the animations don't show up).

--- Assets/Scripts/MockScene/Item/HealthyFood.cs
+++ Assets/Scripts/MockScene/Item/HealthyFood.cs
@@ -12,29 +12,65 @@ public class HealthyFood : Item {
 	public Sprite[] tomato;
 	public Sprite[] watermelon;
 
+	// Christmas reskin
+	public Sprite[] redPresent;
+	public Sprite[] bluePresent;
+	public Sprite[] greenPresent;
+	public Sprite[] yellowPresent;
+	public Sprite[] darkGreenPresent;
+	public Sprite[] purplePresent;
+	public Sprite[] pinkPresent;
+	public Sprite[] orangePresent;
+
+	// Sets whether to use the Christmas reskin - temporary solution? Could set some other way - e.g. a separate script
+	public enum Theme {endless, xmas, story}
+	public static Theme CURRENT_THEME = Theme.story; // Should be set from another script
+
 	// Use this for initialization
 	void Start () {
 		
 		int var = Random.Range (0, 80);
-		
-		if (var < 10) {
-			sprites = apple;
-		} else if (var < 20) {
-			sprites =  banana;
-		} else if (var < 30) {
-			sprites =  carrot;
-		} else if (var < 40) {
-			sprites =  eggplant;
-		} else if (var < 50) {
-			sprites =  orange;
-		} else if (var < 60) {
-			sprites =  pear;
-		} else if (var < 70) {
-			sprites =  tomato;
-		} else if (var <= 80) {
-			sprites =  watermelon;
-		} 
-	
+
+		// Using the standard theme
+		if (CURRENT_THEME == Theme.story) {
+			if (var < 10) {
+				sprites = apple;
+			} else if (var < 20) {
+				sprites = banana;
+			} else if (var < 30) {
+				sprites = carrot;
+			} else if (var < 40) {
+				sprites = eggplant;
+			} else if (var < 50) {
+				sprites = orange;
+			} else if (var < 60) {
+				sprites = pear;
+			} else if (var < 70) {
+				sprites = tomato;
+			} else if (var <= 80) {
+				sprites = watermelon;
+			} 
+
+		// Using the christmas theme
+		} else if (CURRENT_THEME == Theme.xmas) { 
+			if (var < 10) {
+				sprites = redPresent;
+			} else if (var < 20) {
+				sprites = bluePresent;
+			} else if (var < 30) {
+				sprites = greenPresent;
+			} else if (var < 40) {
+				sprites = yellowPresent;
+			} else if (var < 50) {
+				sprites = orangePresent;
+			} else if (var < 60) {
+				sprites = darkGreenPresent;
+			} else if (var < 70) {
+				sprites = purplePresent;
+			} else if (var <= 80) {
+				sprites = pinkPresent;
+			} 
+		}
 	}
 	
 	// Update is called once per frame
--- Assets/Scripts/MockScene/Item/JunkFood.cs
+++ Assets/Scripts/MockScene/Item/JunkFood.cs
@@ -10,23 +10,54 @@ public class JunkFood : Item {
 	public Sprite[] cake;
 	public Sprite[] icecream;
 
+	// Christmas reskin
+	public Sprite[] candyCane;
+	public Sprite[] pavlova;
+	public Sprite[] pudding;
+	public Sprite[] snowmanBiscuit;
+	public Sprite[] mincePie;
+	public Sprite[] gingerbreadMan;
+
+	// Sets whether to use the Christmas reskin - temporary solution? Could set some other way - e.g. a separate script
+	public enum Theme {endless, xmas, story}
+	public static Theme CURRENT_THEME = Theme.story; // Should be set from another script
+
 	// Use this for initialization
 	void Start () { 
 		
 		int var = Random.Range (0, 60);
 
-		if (var < 10) {
-			sprites = burger;
-		} else if (var < 20) {
-			sprites = candy;
-		} else if (var < 30) {
-			sprites = chocolate;
-		} else if (var < 40) {
-			sprites = cupcake;
-		} else if (var < 50) {
-			sprites = cake;
-		} else if (var <= 60) {
-			sprites = icecream;
+		// Using the standard theme
+		if (CURRENT_THEME == Theme.story) {
+			if (var < 10) {
+				sprites = burger;
+			} else if (var < 20) {
+				sprites = candy;
+			} else if (var < 30) {
+				sprites = chocolate;
+			} else if (var < 40) {
+				sprites = cupcake;
+			} else if (var < 50) {
+				sprites = cake;
+			} else if (var <= 60) {
+				sprites = icecream;
+			}
+		
+		// Using the Christmas theme
+		} else if (CURRENT_THEME == Theme.xmas) {
+			if (var < 10) {
+				sprites = pavlova;
+			} else if (var < 20) {
+				sprites = pudding;
+			} else if (var < 30) {
+				sprites = mincePie;
+			} else if (var < 40) {
+				sprites = candyCane;
+			} else if (var < 50) {
+				sprites = cake;
+			} else if (var <= 60) {
+				sprites = gingerbreadMan;
+			}
 		}
 	}
 	
