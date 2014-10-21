4083b1c5de94fdac5c9c8ddc802fc0dd248acc01
matthew W <bwan210@aucklanduni.ac.nz>
2014-10-22 01:39:37 +1300

    hard coded hooks removed

--- Assets/Scripts/Factory/EndlessGameObjectFactory.cs
+++ Assets/Scripts/Factory/EndlessGameObjectFactory.cs
@@ -34,70 +34,15 @@ public class EndlessGameObjectFactory : GameObjectFactory {
 
 			y += 3f;
 		}
+	}
 
-//		hardCodedLevelStartHook ();
-		}
-
-	private void hardCodedLevelStartHook(){
-
-
-		currentPlatform = (GameObject) Instantiate(Resources.Load ("Prefabs/Platforms/" + "pref_standard_platform"));
-		Vector3 temp = new Vector3(2,2,0);
-		currentPlatform.transform.position += temp;
-
-		currentPlatform = (GameObject) Instantiate(Resources.Load ("Prefabs/Platforms/" + "pref_standard_platform"));
-		temp = new Vector3(-2,4,0);
-		currentPlatform.transform.position += temp;
-
-		currentPlatform = (GameObject) Instantiate(Resources.Load ("Prefabs/Platforms/" + "pref_standard_platform"));
-		temp = new Vector3(2,6,0);
-		currentPlatform.transform.position += temp;
-
-		currentPlatform = (GameObject) Instantiate(Resources.Load ("Prefabs/Platforms/" + "pref_standard_platform"));
-		temp = new Vector3(-2,8,0);
-		currentPlatform.transform.position += temp;
-
-		currentPlatform = (GameObject) Instantiate(Resources.Load ("Prefabs/Platforms/" + "pref_standard_platform"));
-		temp = new Vector3(2,10,0);
-		currentPlatform.transform.position += temp;
-
-		currentPlatform = (GameObject) Instantiate(Resources.Load ("Prefabs/Platforms/" + "pref_standard_platform"));
-		temp = new Vector3(-2,12,0);
-		currentPlatform.transform.position += temp;
-
-		currentPlatform = (GameObject) Instantiate(Resources.Load ("Prefabs/Platforms/" + "pref_standard_platform"));
-		temp = new Vector3(2,14,0);
-		currentPlatform.transform.position += temp;
-
-		currentPlatform = (GameObject) Instantiate(Resources.Load ("Prefabs/Platforms/" + "pref_standard_platform"));
-		temp = new Vector3(-2,16,0);
-		currentPlatform.transform.position += temp;
-
-		currentPlatform = (GameObject) Instantiate(Resources.Load ("Prefabs/Platforms/" + "pref_standard_platform"));
-		temp = new Vector3(2,18,0);
-		currentPlatform.transform.position += temp;
-
-		currentPlatform = (GameObject) Instantiate(Resources.Load ("Prefabs/Platforms/" + "pref_standard_platform"));
-		temp = new Vector3(-2,20,0);
-		currentPlatform.transform.position += temp;
-
-	
-		}
 
 	public override void generateTick(){
 		rng.generateNextState ();
 		generateOneTickPlatforms (18.0f);
 
-			//hardCodedGenerateTickHook ();
-		}
+	}
 
-	private void hardCodedGenerateTickHook(){	
-			this.newPlatform = (GameObject)Instantiate (Resources.Load ("Prefabs/Platforms/" + "pref_standard_platform"));
-			this.newPlatform.transform.position = new Vector3 (2, 18, 0);
-
-			this.newPlatform = (GameObject)Instantiate (Resources.Load ("Prefabs/Platforms/" + "pref_standard_platform"));
-			this.newPlatform.transform.position = new Vector3 (-2, 20, 0);
-			}
 
 	private float generateOneTickPlatforms(float y){
 		float currentX = 0.0f;
