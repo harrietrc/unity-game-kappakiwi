499560d098b77bd24762d0e08dc483bb9b2291f7
Matthew W <bwan210@aucklanduni.ac.nz>
2014-10-22 20:59:36 +1300

    worked on difficultymanager

--- Assets/Scripts/RNG/Difficulty/DifficultyManager.cs
+++ Assets/Scripts/RNG/Difficulty/DifficultyManager.cs
@@ -28,4 +28,278 @@ public class DifficultyManager {
 			return instance;
 		}
 	}
+
+	public RNGState.itemType createItemType(){
+		switch (CURRENT_ITEMCOUNT) {
+			case ItemCount.low:
+				int index = 0;
+				index = Random.Range(0,7);
+				switch(index){
+				case 0:
+					return RNGState.itemType.healthy;
+					break;
+				case 1:
+					return RNGState.itemType.junk;
+					break;
+				case 2:
+					return RNGState.itemType.none;
+					break;
+				case 3:
+					return RNGState.itemType.none;
+					break;
+				case 4:
+					return RNGState.itemType.none;
+					break;
+				case 5:
+					return RNGState.itemType.none;
+					break;
+				case 6:
+					return RNGState.itemType.none;
+					break;
+				}
+				break;
+			case ItemCount.medium:
+				index = Random.Range(0,5);
+				switch(index){
+				case 0:
+					return RNGState.itemType.healthy;
+					break;
+				case 1:
+					return RNGState.itemType.junk;
+					break;
+				case 2:
+					return RNGState.itemType.none;
+					break;
+				case 3:
+					return RNGState.itemType.none;
+					break;
+				case 4:
+					return RNGState.itemType.none;
+					break;
+				}
+				break;
+		case ItemCount.high:
+			index = Random.Range(0,3);
+				switch(index){
+				case 0:
+					return RNGState.itemType.healthy;
+					break;
+				case 1:
+					return RNGState.itemType.junk;
+					break;
+				case 2:
+					return RNGState.itemType.none;
+					break;
+				}
+			break;
+	}
+
+	
+	}
+
+	public RNGState.enemyType createEnemyType(){
+		switch (CURRENT_ENEMYCOUNT) {
+		case EnemyCount.low:
+			int index = 0;
+			index = Random.Range(0,13);
+			switch(index){
+			case 0:
+				return RNGState.enemyType.falling;
+				break;
+			case 1:
+				return RNGState.enemyType.patrol;
+				break;
+			case 2:
+				return RNGState.enemyType.shooting;
+				break;
+			case 3:
+				return  RNGState.enemyType.stationary;
+				break;
+			case 4:
+				return RNGState.enemyType.none;
+				break;
+			case 5:
+				return RNGState.enemyType.none;
+				break;
+			case 6:
+				return RNGState.enemyType.none;
+				break;
+			case 8:
+				return RNGState.enemyType.none;
+				break;
+			case 9:
+				return RNGState.enemyType.none;
+				break;
+			case 10:
+				return RNGState.enemyType.none;
+				break;
+			case 11:
+				return RNGState.enemyType.none;
+				break;
+			case 12:
+				return RNGState.enemyType.none;
+				break;
+				
+			}
+			break;
+		case ItemCount.medium:
+			index = 0;
+			index = Random.Range(0,8);
+			switch(index){
+			case 0:
+				return RNGState.enemyType.falling;
+				break;
+			case 1:
+				return RNGState.enemyType.patrol;
+				break;
+			case 2:
+				return RNGState.enemyType.shooting;
+				break;
+			case 3:
+				return  RNGState.enemyType.stationary;
+				break;
+			case 4:
+				return RNGState.enemyType.none;
+				break;
+			case 5:
+				return RNGState.enemyType.none;
+				break;
+			case 6:
+				return RNGState.enemyType.none;
+				break;
+			case 7:
+				return RNGState.enemyType.none;
+				break;
+				
+			}
+			break;
+		case ItemCount.high:
+			index = 0;
+			index = Random.Range(0,6);
+			switch(index){
+			case 0:
+				return RNGState.enemyType.falling;
+				break;
+			case 1:
+				return RNGState.enemyType.patrol;
+				break;
+			case 2:
+				return RNGState.enemyType.shooting;
+				break;
+			case 3:
+				return  RNGState.enemyType.stationary;
+				break;
+			case 4:
+				return RNGState.enemyType.none;
+				break;
+			case 5:
+				return RNGState.enemyType.none;
+				break;
+			}
+			break;
+		}
+	}
+
+	public RNGState.platformType createPlatformType(){
+		switch (CURRENT_ENEMYCOUNT) {
+		case EnemyCount.low:
+			int index = 0;
+			index = Random.Range(0,13);
+			switch(index){
+			case 0:
+				return RNGState.enemyType.falling;
+				break;
+			case 1:
+				return RNGState.enemyType.patrol;
+				break;
+			case 2:
+				return RNGState.enemyType.shooting;
+				break;
+			case 3:
+				return  RNGState.enemyType.stationary;
+				break;
+			case 4:
+				return RNGState.enemyType.none;
+				break;
+			case 5:
+				return RNGState.enemyType.none;
+				break;
+			case 6:
+				return RNGState.enemyType.none;
+				break;
+			case 8:
+				return RNGState.enemyType.none;
+				break;
+			case 9:
+				return RNGState.enemyType.none;
+				break;
+			case 10:
+				return RNGState.enemyType.none;
+				break;
+			case 11:
+				return RNGState.enemyType.none;
+				break;
+			case 12:
+				return RNGState.enemyType.none;
+				break;
+				
+			}
+			break;
+		case ItemCount.medium:
+			index = 0;
+			index = Random.Range(0,8);
+			switch(index){
+			case 0:
+				return RNGState.enemyType.falling;
+				break;
+			case 1:
+				return RNGState.enemyType.patrol;
+				break;
+			case 2:
+				return RNGState.enemyType.shooting;
+				break;
+			case 3:
+				return  RNGState.enemyType.stationary;
+				break;
+			case 4:
+				return RNGState.enemyType.none;
+				break;
+			case 5:
+				return RNGState.enemyType.none;
+				break;
+			case 6:
+				return RNGState.enemyType.none;
+				break;
+			case 7:
+				return RNGState.enemyType.none;
+				break;
+				
+			}
+			break;
+		case ItemCount.high:
+			index = 0;
+			index = Random.Range(0,6);
+			switch(index){
+			case 0:
+				return RNGState.enemyType.falling;
+				break;
+			case 1:
+				return RNGState.enemyType.patrol;
+				break;
+			case 2:
+				return RNGState.enemyType.shooting;
+				break;
+			case 3:
+				return  RNGState.enemyType.stationary;
+				break;
+			case 4:
+				return RNGState.enemyType.none;
+				break;
+			case 5:
+				return RNGState.enemyType.none;
+				break;
+			}
+			break;
+		}
+	}
 }
