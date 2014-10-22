cef64f718519ee7570dcd4a4b56bd3f538820ed4
Eric <mark_930511@hotmail.com>
2014-10-22 14:14:46 +1300

    git rid of stationary enemies

--- Assets/Scripts/Common/CameraAspectController.cs
+++ Assets/Scripts/Common/CameraAspectController.cs
@@ -5,7 +5,7 @@ public class CameraAspectController : MonoBehaviour {
 	private float height;
 	private float width;
 
-	private bool enabled = true;
+	private bool enabled = false;
 	// Use this for initialization
 	void Start()
 	{
