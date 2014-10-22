cebd566608f719f8c33af3e0dc97edc2870bbaaa
Sean <toyman93@gmail.com>
2014-10-22 23:30:17 +1300

    Merging highscore screen

--- Assets/Scripts/Screens/HighScoreScript.cs
+++ Assets/Scripts/Screens/HighScoreScript.cs
@@ -19,21 +19,18 @@ public class HighScoreScript : MonoBehaviour {
 
 		GUIStyle style = new GUIStyle (GUI.skin.label);
 		style.font = (Font)Resources.Load ("font/Animated");
-		style.fontSize = 24;
+		style.fontSize = 18;
 		style.normal.textColor = Color.black;
 
+		GUI.Label (new Rect(Screen.width*0.1f, Screen.height * 0.2f, Screen.width*0.9f, Screen.height * 0.2f), "Your new high score is : ", style);
 
-
-		GUI.Label (new Rect(Screen.width * 0.35f, Screen.height * 0.05f, Screen.width * 0.8f, Screen.height * 0.2f), "New Highscore!", style);
-		GUI.Label (new Rect(Screen.width * 0.35f, Screen.height * 0.20f, Screen.width * 0.8f, Screen.height * 0.2f), PlayerPrefs.GetInt ("LastScore").ToString (), style);
-
-		this.name = GUI.TextField (new Rect (Screen.width * 0.35f, Screen.height * 0.35f, Screen.width * 0.3f, Screen.height * 0.1f), name, 10);
-
+		this.name = GUI.TextField (new Rect (Screen.width * 0.2f, Screen.height * 0.65f, Screen.width * 0.6f, 20), name, 10);
+	
 		PlayerPrefs.SetString ("NewName", this.name);
 
 		Debug.Log (PlayerPrefs.GetString ("NewName"));
 
-		if (GUI.Button (new Rect (100, 150, 300, 50), "Continue")) {
+		if (GUI.Button (new Rect (Screen.width * 0.2f, Screen.height * 0.8f, Screen.width*0.6f, 50), "Continue")) {
 
 
 			if (PlayerPrefs.GetInt ("Finished") == 0) {
