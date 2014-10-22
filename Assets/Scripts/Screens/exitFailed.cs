33a71bec0ad13cc182ee4e518b0e80b6233c70b5
Earl Balmes <ebal155@aucklanduni.ac.nz>
2014-10-22 15:17:39 +1300

    Reformatted exitFailed screen

--- Assets/Scripts/Screens/exitFailed.cs
+++ Assets/Scripts/Screens/exitFailed.cs
@@ -1,6 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
-
+using System.Collections.Generic;
 public class exitFailed : MonoBehaviour {
 
 	private PlayerStatus playerStatus = new PlayerStatus();
@@ -10,6 +10,10 @@ public class exitFailed : MonoBehaviour {
 	private GameObject highscoretext3;
 	private GameObject highscoretext4;
 	private GameObject highscoretext5;
+	private List<GameObject> highScoreTextList = new List<GameObject>();
+	private List<int> highScores = new List<int>() ;
+	private List<string> highScoreNames = new List<string>();
+	
 	private GameObject textfield;
 
 	private string name;
@@ -25,66 +29,42 @@ public class exitFailed : MonoBehaviour {
 		playerStatus.displayHighScoreList ();
 		playerStatus.saveHighScoresToPersistence ();
 
+		highScoreTextList.Add (highscoretext1);
+		highScoreTextList.Add (highscoretext2);
+		highScoreTextList.Add (highscoretext3);
+		highScoreTextList.Add (highscoretext4);
+		highScoreTextList.Add (highscoretext5);
+
+		highScoreNames.Add(PlayerPrefs.GetString ("HighScore1"));
+		highScoreNames.Add(PlayerPrefs.GetString ("HighScore2"));
+		highScoreNames.Add(PlayerPrefs.GetString ("HighScore3"));
+		highScoreNames.Add(PlayerPrefs.GetString ("HighScore4"));
+		highScoreNames.Add(PlayerPrefs.GetString ("HighScore5"));
+		
+		highScores.Add(PlayerPrefs.GetInt (highScoreNames[0]));
+		highScores.Add(PlayerPrefs.GetInt (highScoreNames[1]));
+		highScores.Add(PlayerPrefs.GetInt (highScoreNames[2]));
+		highScores.Add(PlayerPrefs.GetInt (highScoreNames[3]));
+		highScores.Add(PlayerPrefs.GetInt (highScoreNames[4]));
+
+		float tempX = 0.7f;
+		float tempY = 0.8f;
+		GameObject h;
+	
+		for (int i = 0; i < 5; i++) {
+			h = highScoreTextList[i];
+			h = new GameObject();
+			h.AddComponent<GUIText>();
+			h.transform.position = new Vector3(tempX,tempY,0);
+			h.guiText.text = (i+1) + ": " + highScoreNames[i] + " " + highScores[i];
+			h.guiText.material.color = Color.black;
+			tempY -= 0.1f;
+		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		//if (this.name != null) {
-		//	playerStatus.updateHighScoreList (this.name);
-		//}
 
-		//this.name = GUI.TextField (new Rect (Screen.width * 0.35f, Screen.height * 0.35f, Screen.width * 0.8f, Screen.height * 0.2f), name, 25, scoreStyle);
-
-		string hiName1 = PlayerPrefs.GetString ("HighScore1");
-		int highscore1 = PlayerPrefs.GetInt (hiName1);
-		string hiName2 = PlayerPrefs.GetString ("HighScore2");
-		int highscore2 = PlayerPrefs.GetInt (hiName2);
-		string hiName3 = PlayerPrefs.GetString ("HighScore3");
-		int highscore3 = PlayerPrefs.GetInt (hiName3);
-		string hiName4 = PlayerPrefs.GetString ("HighScore4");
-		int highscore4 = PlayerPrefs.GetInt (hiName4);
-		string hiName5 = PlayerPrefs.GetString ("HighScore5");
-		int highscore5 = PlayerPrefs.GetInt (hiName5);
-
-		if (highscoretext1 == null) {
-			highscoretext1 = new GameObject ();
-			highscoretext1.AddComponent<GUIText> ();
-			highscoretext1.transform.position = new Vector3 (0.5f, 0.9f, 0);
-			highscoretext1.guiText.text = "1: " + hiName1 + " " + highscore1;
-			highscoretext1.guiText.material.color = Color.black;
-		}
-
-		if (highscoretext2 == null) {
-			highscoretext2 = new GameObject ();
-			highscoretext2.AddComponent<GUIText> ();
-			highscoretext2.transform.position = new Vector3 (0.5f, 0.8f, 0);
-			highscoretext2.guiText.text = "2: " + hiName2 + " " + highscore2;
-			highscoretext2.guiText.material.color = Color.black;
-		}
-
-		if (highscoretext3 == null) {
-			highscoretext3 = new GameObject ();
-			highscoretext3.AddComponent<GUIText> ();
-			highscoretext3.transform.position = new Vector3 (0.5f, 0.7f, 0);
-			highscoretext3.guiText.text = "3: " + hiName3 + " " + highscore3;
-			highscoretext3.guiText.material.color = Color.black;
-		}
-
-		if (highscoretext4 == null) {
-			highscoretext4 = new GameObject ();
-			highscoretext4.AddComponent<GUIText> ();
-			highscoretext4.transform.position = new Vector3 (0.5f, 0.6f, 0);
-			highscoretext4.guiText.text = "4: " + hiName4 + " " + highscore4;
-			highscoretext4.guiText.material.color = Color.black;
-		}
-
-		if (highscoretext5 == null) {
-			highscoretext5 = new GameObject ();
-			highscoretext5.AddComponent<GUIText> ();
-			highscoretext5.transform.position = new Vector3 (0.5f, 0.5f, 0);
-			highscoretext5.guiText.text = "5: " + hiName5 + " " + highscore5;
-			highscoretext5.guiText.material.color = Color.black;
-		}
 	}
 
 	void OnGUI(){
@@ -94,11 +74,7 @@ public class exitFailed : MonoBehaviour {
 		style.fontSize = 30;
 		style.normal.textColor = Color.black;
 
-		GUIStyle scoreStyle = new GUIStyle (GUI.skin.label);
-		
-		scoreStyle.font = (Font)Resources.Load ("font/Animated");
-		scoreStyle.fontSize = 12;
-		scoreStyle.normal.textColor = Color.black;
+		GUI.Label (new Rect(Screen.width * 0.7f, Screen.height * 0.05f, Screen.width * 0.8f, Screen.height * 0.1f), "Highscores", style);
 
 		GUI.Label (new Rect(Screen.width * 0.1f, Screen.height * 0.05f, Screen.width * 0.8f, Screen.height * 0.2f), "Game Over", style);
 
